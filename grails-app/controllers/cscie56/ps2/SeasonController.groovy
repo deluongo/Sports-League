e5f0bd7c63125e296804c7e183f739d36cd65601
Devon Luongo <deluongo@gmail.com>
2017-02-16 04:42:41 -0800

    Controller Fix

--- grails-app/controllers/cscie56/ps2/SeasonController.groovy
+++ grails-app/controllers/cscie56/ps2/SeasonController.groovy
@@ -1,6 +1,5 @@
 package cscie56.ps2
 
-
 class SeasonController {
 
     static scaffold = Season
--- grails-app/domain/cscie56/ps2/Team.groovy
+++ grails-app/domain/cscie56/ps2/Team.groovy
@@ -1,35 +1,20 @@
 package cscie56.ps2
 
-
-/*---------------------------------------------------------------------------------------------*
-* ===========================================
-* CLASS -> TEAM!
-* ===========================================
-* PROPERTIES:
-*     - name, streak, wins, losses, ties, scored, allowed, delta, winPercent, lastResult, result
-* METHODS:
-*     - wins() -> Updates Team wins, result & last result
-*     - loses() -> Updates Team losses
-*     - ties() -> Updates Team ties
-*     - scores() -> Updates Team's total points scored
-*     - allowed() -> Updates Team total points allowed
-*     - calcDelta() -> Calculates Team's point differential
-*     - calcWinPercent() -> Calculates Team's winning percentage
-*     - calcStreak() -> Calculates the teams winning or loosing streaks
-/*---------------------------------------------------------------------------------------------*/
 class Team {
-	/* ~~~~~~~~~~~~~~ PROPERTIES ~~~~~~~~~~~~~ */
-	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-	String name, streak
-	Integer wins, losses, ties, scored, allowed, delta
-	BigDecimal winPercent
-	Character lastResult, result
+
+    Integer wins
+    Integer losses
+    Integer ties
+    Double winPercent
+    Integer pointsScored
+    Integer pointsAgainst
+    Integer streak
     def coachingStaff
     def roster
-    Integer id
 
-	/* ~~~~~~~~~~~~~~~ 1-to-Many ~~~~~~~~~~~~~ */
-	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    Integer id
+	String name
+
     static hasMany = [persons:Person]
 
 
