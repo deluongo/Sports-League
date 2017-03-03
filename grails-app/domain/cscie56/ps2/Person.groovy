347af2d851efa5183afcac1f05a38adbd43ee92a
Devon Luongo <deluongo@gmail.com>
2017-03-03 11:54:39 -0800

    Updated Person Domain Class

--- grails-app/domain/cscie56/ps2/Person.groovy
+++ grails-app/domain/cscie56/ps2/Person.groovy
@@ -2,14 +2,27 @@ package cscie56.ps2
 
 class Person {
 
+    //PS2
     String firstName
     String lastName
     def number
     String role
     Integer pointsScored
 
+    //PS3
+    String bio
+    Date birthDate
+    String birthPlace
+    String height // must be displayed in feet and inches (e.g., 6’1”)
+    Integer weight
+    String universityAttended
+
     static belongsTo = [team:Team]
 
     static constraints = {
+    }
+
+    static mapping = {
+        bio type: 'longtext'
     }
 }
