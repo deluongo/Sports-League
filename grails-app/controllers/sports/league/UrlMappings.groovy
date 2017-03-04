3677ffdb7140087b35eb67fe8690c2e6f628d203
Devon Luongo <deluongo@gmail.com>
2017-03-03 16:58:47 -0800

    Created Tag Lib

--- grails-app/controllers/sports/league/UrlMappings.groovy
+++ grails-app/controllers/sports/league/UrlMappings.groovy
@@ -14,6 +14,7 @@ class UrlMappings {
             action = 'showStandings'
         }
 
+
         "/league/season/showStandings/$conferenceIndex?" {
             controller = 'league'
             action = 'season'
---
+++ grails-app/views/sharedTemplates/_gameStatsRow.gsp
@@ -0,0 +1,5 @@
+
+<div class="player" id="${player?.id}">
+    <stats:seasonStats player="${this.person}" ></stats:seasonStats>
+    <stats:perGameStats player="${player}" ></stats:perGameStats>
+</div>
